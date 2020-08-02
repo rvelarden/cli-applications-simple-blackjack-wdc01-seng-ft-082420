254d8634206e6d40a37459f7b028832b48daa751
Romina Velarde <rominapaola.velarde@gmail.com>
2020-08-02 15:52:12 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,73 @@
+require 'pry'
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+ array = [1,2,3,4,5,6,7,8,9,10,11]
+array.sample
+   
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  
+  puts "Your cards add up to #{card_total}"
+  
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets 
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  
+card_total
+ 
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  respond = get_user_input
+  while respond != 'h' && respond != 's' 
+  invalid_command
+   prompt_user
+  respond = get_user_input
+  end
+    if respond == 's'
+      nil 
+      
+    elsif respond == 'h'
+      card_total += deal_card
+      
+  end
+card_total 
 end
+
+# prompt_user 
+# respond = get_user_input    
+# until respond == 'h'|| respond == 's' 
+# invalid_command
+# prompt_user 
+# respond = get_user_input
+# end
+# if respond == 'h'
+# card_total += deal_card
+# end 
+#   card_total 
+# end
 
 def invalid_command
-  # code invalid_command here
+puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +75,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
-end
-    
+welcome
+card_total = initial_round
+ until card_total > 21
+card_total = hit?(card_total)
+display_card_total(card_total)
+ end
+end_game(card_total)
+end    
